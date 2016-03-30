b8905cef48ffc49d2168e47c5a1df6a1f613f211
Jonathan Fuqua <jfuqua390@gmail.com>
2016-03-30 16:27:17 -0500

    GOT BOTH TO SHOW SOMEHOW

--- src/app.js
+++ src/app.js
@@ -1,48 +1,18 @@
+import PuppyView from 'puppy-view';
 
 export default class ApplicationView {
   constructor() {
-    let puppyinfo = [];
+    this.puppyinfo = [];
   fetch(`http://tiny-tn.herokuapp.com/collections/ryan-puppy`)
     .then(r => r.json())
     .then(data => {
       console.log(data); //delete after working
       data.forEach(puppy => {
-        this.name = puppy.name;
-        this.age = puppy.age;
-        this.photoUrl = puppy.photoUrl;
-        this.profile = puppy.profile;
-        puppyinfo.push(this.name, this.age, this.photoUrl, this.profile); //pushes all details to puppyinfo array
-        this.render();
-      });
+        console.log(puppy); //delete after complete
+        let x = new PuppyView(puppy._id, puppy.name, puppy.age, puppy.photoUrl, puppy.profile);
+        // console.log(x);
+      })
     })
   }
 
-  render(){
-    let pupcard = document.querySelector('.puppy-card');
-    pupcard.innerHTML = ``;
-    pupcard.innerHTML = `
-    <div class="puppy-card__image">
-      <img src="${this.photoUrl}" alt="" className="puppy-card__pic"/>
-    </div>
-    <div class="puppy-card__form">
-      <ul class="puppy-card__form-inputs">
-        <li class="puppy-card__form-inputs-item">
-          <h6 class="inputs-item__label">Name</h6>
-          <input type="text" class="inputs-item__input" value="${this.name}">
-        </li>
-        <li class="puppy-card__form-inputs-item">
-          <h6 class="inputs-item__label">Age</h6>
-          <input type="text" class="inputs-item__input" value="${this.age}">
-        </li>
-        <li class="puppy-card__form-inputs-item">
-          <h6 class="inputs-item__label">Photo URL</h6>
-          <input type="text" class="inputs-item__input" value="${this.photoUrl}">
-        </li>
-        <li class="puppy-card__form-inputs-item">
-          <h6 class="inputs-item__label">Profile</h6>
-          <input type="text" class="inputs-item__input" value="${this.profile}">
-        </li>
-      </ul>
-    </div>`
   }
-}
---
+++ src/puppy-view.js
@@ -0,0 +1,38 @@
+
+export default class PuppyView {
+  constructor(id, name, age, photoUrl, profile) {
+    this.pupcard = document.querySelector('.puppy-card');
+    this.newcard = document.createElement('div');
+    this.newcard.classList.add('puppy-card')
+    this.row = document.querySelector('.row');
+    this.newcard.innerHTML = `
+    <div class="puppy-card__image">
+      <img src="${photoUrl}" alt="" className="puppy-card__pic"/>
+    </div>
+    <div class="puppy-card__form">
+      <ul class="puppy-card__form-inputs">
+        <li class="puppy-card__form-inputs-item">
+          <h6 class="inputs-item__label">Name</h6>
+          <input type="text" class="inputs-item__input" value="${name}">
+        </li>
+        <li class="puppy-card__form-inputs-item">
+          <h6 class="inputs-item__label">Age</h6>
+          <input type="text" class="inputs-item__input" value="${age}">
+        </li>
+        <li class="puppy-card__form-inputs-item">
+          <h6 class="inputs-item__label">Photo URL</h6>
+          <input type="text" class="inputs-item__input" value="${photoUrl}">
+        </li>
+        <li class="puppy-card__form-inputs-item">
+          <h6 class="inputs-item__label">Profile</h6>
+          <input type="text" class="inputs-item__input" value="${profile}">
+        </li>
+      </ul>
+    </div>`;
+    this.row.appendChild(this.newcard);
+    console.log(this.newcard);
+  }
+  renderpup(){
+      }
+
+}
